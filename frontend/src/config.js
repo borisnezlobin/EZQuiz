6cb672869c4b282ebb3881acc8444489ddcbe3a3
Rand0mLetters <me@borisnezlobin.com>
2024-01-30 14:48:22 -0800

    attempt 4! got http and ws working on one port locally, time to test in prod

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,8 @@
+const dev = false;
+
 const CONFIG = {
-    SERVER_URL: "https://ezqz.onrender.com",
-    SOCKET_URL: "ws://ezqz.onrender.com/ws",
+    SERVER_URL: dev ? "http://localhost:8000" : "https://ezqz.onrender.com",
+    SOCKET_URL: dev ? "ws://localhost:8000/ws" : "https://ezqz.onrender.com/ws",
 }
 
 export default CONFIG;
