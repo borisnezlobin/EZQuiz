eea2658ff001657f35c2003d32f9b3bb99982de2
Rand0mLetters <boris.nezlobin@gmail.com>
2024-01-30 10:08:14 -0800

    remove node_modules lmao

--- backend/node_modules/spacy/src/index.js
+++
@@ -1,8 +0,0 @@
-import Language from './language'
-export { Doc, Token, Span } from './tokens'
-
-export default {
-    load: function(model, api) {
-        return new Language(model, api);
-    }
-}
--- backend/node_modules/spacy/tests/language.test.js
+++
@@ -1,8 +0,0 @@
-import spacy from '../src';
-
-jest.mock('../src/language');
-
-test('creates new nlp object', () => {
-    const nlp = spacy.load('en_core_web_sm');
-    expect(nlp).toEqual(expect.any(Function));
-});
